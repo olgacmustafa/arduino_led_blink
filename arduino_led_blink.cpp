7a8fa05250b109511aca5a52cedbf6e54732cf34
Mustafa <olgcmustafa@gmail.com>
2025-05-27 19:48:34 +0300

    Add files via upload

---
+++ arduino_led_blink.cpp
@@ -0,0 +1,21 @@
+int led1=8;
+
+
+void setup(){
+  
+  pinMode(led1,OUTPUT);
+
+  
+}
+void loop(){
+  
+  digitalWrite(led1,HIGH);
+
+  
+  delay(1000);
+  
+  digitalWrite(led1,LOW);
+    
+  delay(1000);
+ 
+}
